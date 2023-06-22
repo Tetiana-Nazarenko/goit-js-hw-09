35c0cb14f82d2490460401a3d37189aaa02c73ec
Tetiana-Nazarenko <123865975+Tetiana-Nazarenko@users.noreply.github.com>
2023-06-22 15:18:47 +0000

    Deploying to gh-pages from @ Tetiana-Nazarenko/goit-js-hw-09@6bff19df6a3cbc45a0512c9a226ee7612f3b4555 🚀

---
+++ 01-color-switcher.7c3876b4.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),a=document.querySelector("button[data-stop]");a.disabled=!0;var n=null;e.addEventListener("click",(function(){n=setInterval((function(){t.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16).padStart(6,0))}),1e3),e.disabled=!0,a.disabled=!1})),a.addEventListener("click",(function(){clearInterval(n),e.disabled=!1,a.disabled=!0}))}();
+//# sourceMappingURL=01-color-switcher.7c3876b4.js.map
