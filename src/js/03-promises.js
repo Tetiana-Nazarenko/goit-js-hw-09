c1473e9cdd41466d7d26448e716f794d06ad79e3
Tetiana-Nazarenko <nnn27725@gmail.com>
2023-06-27 12:50:25 +0300

    update task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,38 +1,51 @@
 import Notiflix from 'notiflix';
 
-const form = document.querySelectorAll('input');
+const form = document.querySelector('.form');
+console.log(form);
 const delay = document.querySelector('[name="delay"]');
 const step = document.querySelector('[name="step"]');
 const amount = document.querySelector('[name="amount"]');
-const btn = document.querySelector('submit', createPromise);
+
+form.addEventListener('submit', onPromiseCreate);
+
+form.style.display = 'flex';
+form.style.gap = '40px';
+form.style.flexDirection = 'column';
+form.style.alignItems = 'center';
 
 document.body.style.backgroundColor = 'green';
-form.style.alignItem = 'center';
 
 function createPromise(position, delay) {
-  const promise = new Promise((resolve, reject) => {
-    // Asynchronous operation
-    console.log(promise);
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
   });
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-    Notiflix.Report.success('Congratulations!', 'Date is true', 'Continue');
-  } else {
-    Notiflix.Report.failure(
-      'Opps...',
-      'Date is false. Choose another date!',
-      'Ok'
-    );
-    // Reject
+}
+
+function onPromiseCreate(ev) {
+  ev.preventDefault();
+  let delayValue = Number(delay.value);
+  let stepValue = Number(step.value);
+  let amountValue = Number(amount.value);
+
+  for (let i = 1; i <= amountValue; i += 1) {
+    let promiseDelay = delayValue + stepValue * i;
+    createPromise(i, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Report.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Report.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`
+        );
+      });
   }
 }
-//
-
-createPromise(2, 1500)
-  .then(({ position, delay }) => {
-    console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
-  })
-  .catch(({ position, delay }) => {
-    console.log(`❌ Rejected promise ${position} in ${delay}ms`);
-  });
